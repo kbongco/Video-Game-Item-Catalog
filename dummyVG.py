d8fc7f2aecdc8673265c8dae69def1ad31923d0c
Kathleen <whoa.its.chibI@gmail.com>
2019-03-03 18:25:37 -0500

    Removed Company
    Removed company name to reflect changes in the database

--- dummyVG.py
+++ dummyVG.py
@@ -11,7 +11,7 @@ session = DBSession()
 
 #Dummy Video Games Data
 #ICDH = Insert Creative Description Here
-#name, company, coverURL, releaseyear,description, genre, company 
+#name, coverURL, releaseyear,description, genre, 
 
 User1 = User(name ="Chibi", email="whoa.its.chibi@gmail.com")
 session.add(User1)
@@ -24,21 +24,21 @@ session.commit()
 
 VideoGame1 = VideoGames(name="Overwatch", 
 	description = "You control several heroes shooting objectives!",
-	company = "Blizzard Entertainment", releaseyear = '2016',
+	releaseyear = '2016',
 	platform = platform1)
 
 session.add(VideoGame1)
 session.commit()
 
 VideoGame2 = VideoGames(name="League of Legends", 
-	description = "similar to DoTA", company = "Riot Games",
+	description = "similar to DoTA",
 	releaseyear = "2009", platform = platform1)
 
 session.add(VideoGame2)
 session.commit()
 
 VideoGame3 = VideoGames(name = "Five Nights at Freddy's",
-	description ="5 nights, will you survive?", company = "Scott Games", 
+	description ="5 nights, will you survive?",  
 	releaseyear = '2014', platform = platform1)
 
 session.add(VideoGame3)
@@ -50,20 +50,20 @@ session.add(platform2)
 session.commit()
 
 VideoGame1 = VideoGames(name = "Kingdom Hearts 2.8", 
-	description = "Final Chapter before KH3!", company = "Square Enix", 
+	description = "Final Chapter before KH3!", 
 	releaseyear = "2017", platform = platform2)
 
 session.add(VideoGame1)
 session.commit()
 
 VideoGame2 = VideoGames(name = "Ryu Ga Gotoku: 0", 
-	description = "Japan 1988.", company = "Sega", 
+	description = "Japan 1988.", 
 	releaseyear = "2017",  platform = platform2)
 session.add(VideoGame2)
 session.commit()
 
 VideoGame3 = VideoGames(name = "Final Fantasy XV", 
-	description = "The Fifteenth main installment!", company = "Square Enix",
+	description = "The Fifteenth main installment!", 
 	releaseyear = "2016", platform = platform2)
 session.add(VideoGame3)
 session.commit()
@@ -75,13 +75,13 @@ session.commit()
 
 VideoGame1 = VideoGames(name ="Halo:Combat Evolved", 
 	description = "An interstellar war between Humans and the Covenant", 
-	company = "Microsoft", releaseyear = "2001",  
+	releaseyear = "2001",  
 	platform = platform3)
 session.add(VideoGame1)
 session.commit()
 
 VideoGame2 = VideoGames(name = "Fable", description = 
-	"ICDH", company = "Microsoft", releaseyear = "2004", 
+	"Choose your path", releaseyear = "2004", 
 	platform = platform3)
 session.add(VideoGame2)
 session.commit()
@@ -92,18 +92,18 @@ session.add(platform4)
 session.commit()
 
 VideoGame1 = VideoGames(name = "Super Mario Bros", 
-	description = "The Original", company = "Nintendo", releaseyear = "1985",
+	description = "The Original", releaseyear = "1985",
 	platform = platform4)
 session.add(VideoGame1)
 session.commit()
 
 VideoGame2 = VideoGames(name = "Super Smash Bros: Melee", 
-	description = "Nintendo's all stars are back!", company = "Nintendo",
+	description = "Nintendo's all stars are back!",
 	releaseyear = "2003", platform = platform4)
 session.add(VideoGame2)
 session.commit()
 
 VideoGame3 = VideoGames(name = "Mario Party", description ="A party game that destroys friendships!",
-	company ="Nintendo", releaseyear = "1998", platform = platform4)
+	releaseyear = "1998", platform = platform4)
 
 print("Games and Platforms Added!")
