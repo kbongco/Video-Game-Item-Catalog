c40a3dddc38775902f37e22f2c2d9abf07655ad9
Kathleen <whoa.its.chibI@gmail.com>
2019-03-07 13:08:41 -0500

    Updated database_setup.py
    Updated formatting according to PEP8

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,50 +7,54 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-	__tablename__ = 'user'
 
-	id = Column(Integer, primary_key = True)
-	name = Column(String(250), nullable = False)
-	email = Column(String(250), nullable = False)
-	picture = Column(String(250))
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 
 class Platform(Base):
-	__tablename__ = "Platform"
+    __tablename__ = "Platform"
 
-	id =Column(Integer,primary_key = True)
-	name = Column(String(50), nullable = False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property 
-	def serialize(self):
-		return {
-			'id':self.id,
-			'name':self.name,
-			'user_id':self.user_id
-		}
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
 
 class VideoGames(Base):
-	__tablename__ ='video_games'
-	id = Column(Integer, primary_key = True)
-	name = Column(String(80), nullable = False)
-	releaseyear = Column(String(80), nullable = False)
-	description = Column(String(250), nullable = False)
-	platform_id = Column(Integer, ForeignKey('Platform.id'))
-	platform = relationship(Platform)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    __tablename__ = 'video_games'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    releaseyear = Column(String(80), nullable=False)
+    description = Column(String(250), nullable=False)
+    platform_id = Column(Integer, ForeignKey('Platform.id'))
+    platform = relationship(Platform)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property 
-	def serialize(self):
-		return {
-			'name':self.name,
-			'id':self.id,
-			'releaseyear':self.releaseyear,
-			'description':self.description,
-			'platform': self.platform.name,
-		}
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+            'releaseyear': self.releaseyear,
+            'description': self.description,
+            'platform': self.platform.name,
+        }
+
 
 engine = create_engine('sqlite:///VideoGamesCatalog.db')
 
